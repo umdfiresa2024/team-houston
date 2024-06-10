161838ffdecaf70ccaf08af9060eafff0633ccd5
JoshuaHildebrand <jhildebr@umd.edu>
2024-06-10 14:29:22 -0400

    Added Roads and created Buffer File for Stations

--- maproads.R
+++ maproads.R
@@ -4,7 +4,19 @@ r<-terra::vect("tl_2021_txharris_roads/tl_2021_48201_roads.shp")
 
 rdf<-as.data.frame(r)
 
-tsp<-subset(r, r$FULLNAME=="Katy Fwy")
+tsp<-subset(r, r$FULLNAME=="N Loop W Svc Rd" | r$FULLNAME == "Southwest Fwy" | r$FULLNAME == "Katy Fwy" | r$FULLNAME == "S Loop W Svc Rd" | r$FULLNAME == "W Loop S Fwy" | r$FULLNAME == "W Loop S Svc Rd" | r$FULLNAME == "Gulf Fwy" | r$FULLNAME == "North Fwy" | r$FULLNAME == "N Loop E Fwy" | r$FULLNAME == "N Loop E Svc Rd" | r$FULLNAME == "South Fwy")
+tsp_df <- as.data.frame(tsp)
 terra::plot(tsp)
-
+r
 l
+#N Loop W Svc Rd
+#Southwest Fwy
+#Katy Fwy
+#S Loop W Svc Rd
+#W Loop S Fwy
+#W Loop S Svc Rd
+#Gulf Fwy
+#North Fwy
+#N Loop E Fwy
+#N Loop E Svc Rd
+
